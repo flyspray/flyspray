dfec793f5252dcf7f5f327869423cdce3463f29d
floele <floele@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2007-03-10 08:32:53 +0000

    Fixed a LIMIT query
    git-svn-id: https://flyspray.svn.sourceforge.net/svnroot/flyspray/branches/0.9.9@1085 2633ddf7-78cb-0310-bf30-a8c8deaca200

--- setup/upgrade/0.9.9/add_data.php
+++ setup/upgrade/0.9.9/add_data.php
@@ -26,7 +26,7 @@ $db->Query("UPDATE {tasks} SET closure_comment='' WHERE closure_comment='0'");
 $db->Query("UPDATE {groups} SET add_to_assignees = '1' WHERE assign_others_to_self =1 ");
 $db->Query("UPDATE {tasks} SET due_date = 0 WHERE due_date = ''");
 $db->Query("UPDATE {groups} SET add_votes = 1 WHERE group_id = 2 OR group_id = 3 OR group_id = 6");
-$db->Query("UPDATE {groups} SET edit_assignments = '1' WHERE group_id =2 LIMIT 1");
+$db->Query("UPDATE {groups} SET edit_assignments = '1' WHERE group_id = 2");
 $db->Query("UPDATE {history} SET event_type = 3 WHERE event_type = 0");
 $db->Query("UPDATE {history} SET event_type = 11 WHERE event_type = 15");
 $db->Query("UPDATE {history} SET event_type = 12 WHERE event_type = 16");
