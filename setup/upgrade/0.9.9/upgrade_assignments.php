2b4006d9985f32de648043d98711f483886e1ae2
floele <floele@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2007-05-12 09:20:38 +0000

    Another upgrade fix
    git-svn-id: https://flyspray.svn.sourceforge.net/svnroot/flyspray/branches/0.9.9@1208 2633ddf7-78cb-0310-bf30-a8c8deaca200

--- setup/upgrade/0.9.9/upgrade_assignments.php
+++ setup/upgrade/0.9.9/upgrade_assignments.php
@@ -5,13 +5,19 @@
    | assignees per task as described in FS#329.  It only needs |
    | to be run once to do the conversion.                      |
    \***********************************************************/
-                         
+
 $check_sql = $db->Query("SELECT task_id, assigned_to
                            FROM {tasks}
                           WHERE assigned_to > '0'");
 
 while ($row = $db->FetchRow($check_sql))
 {
+   $check = $db->Query('SELECT assigned_id FROM {assigned} WHERE task_id = ? AND user_id = ?',
+                       array($row['task_id'], $row['assigned_to']));
+   if ($db->FetchOne($check)) {
+       continue;
+   }
+
    $db->Query('INSERT INTO {assigned}
                            (task_id, user_id)
                     VALUES (?,?)',
