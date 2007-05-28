addc9ad399aff4abe812909ce724c33b939a3530
floele <floele@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2007-05-28 20:28:15 +0000

    Fixed SQL error when searching for last edit date without having last edit column enabled
    git-svn-id: https://flyspray.svn.sourceforge.net/svnroot/flyspray/branches/0.9.9@1251 2633ddf7-78cb-0310-bf30-a8c8deaca200

--- includes/class.backend.php
+++ includes/class.backend.php
@@ -940,11 +940,12 @@ class Backend
             $from   .= ' LEFT JOIN  {votes} vot         ON t.task_id = vot.task_id ';
             $select .= ' COUNT(DISTINCT vot.vote_id)    AS num_votes, ';
         }
-        if (array_get($args, 'search_in_comments') || in_array('comments', $visible) || in_array('lastedit', $visible)) {
+        $search_for_changes = in_array('lastedit', $visible) || array_get($args, 'changedto') || array_get($args, 'changedfrom');
+        if (array_get($args, 'search_in_comments') || in_array('comments', $visible) || $search_for_changes) {
             $from   .= ' LEFT JOIN  {comments} c        ON t.task_id = c.task_id ';
             $select .= ' COUNT(DISTINCT c.comment_id)   AS num_comments, ';
             // in other words: max(c.date_added, t.date_closed, t.date_opened, t.last_edited_time)
-            if (in_array('lastedit', $visible)) {
+            if ($search_for_changes) {
                 $select .= ' CASE WHEN c.date_added>t.date_closed THEN
                                 CASE WHEN c.date_added>t.date_opened THEN CASE WHEN c.date_added > t.last_edited_time THEN c.date_added ELSE t.last_edited_time END ELSE
                                     CASE WHEN t.date_opened > t.last_edited_time THEN t.date_opened ELSE t.last_edited_time END END ELSE
