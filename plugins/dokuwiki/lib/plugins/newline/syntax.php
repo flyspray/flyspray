adb372837b2a93cefc7d3161aaf467bdf1b79132
floele <floele@2633ddf7-78cb-0310-bf30-a8c8deaca200>
2007-06-03 08:58:03 +0000

    - single newlines for dokuwiki - fixed quotes in history - added project selection for mass edit
    git-svn-id: https://flyspray.svn.sourceforge.net/svnroot/flyspray/branches/0.9.9@1270 2633ddf7-78cb-0310-bf30-a8c8deaca200

---
+++ plugins/dokuwiki/lib/plugins/newline/syntax.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Adds simple <br />s
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Florian Schmitz floele at gmail dot com
+ */
+
+if(!defined('DOKU_INC')) define('DOKU_INC',realpath(dirname(__FILE__).'/../../').'/');
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'syntax.php');
+
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_newline extends DokuWiki_Syntax_Plugin {
+
+    /**
+     * return some info
+     */
+    function getInfo(){
+        return array(
+            'author' => 'Florian Schmitz',
+            'email'  => 'floele@gmail.com',
+            'date'   => '2005-12-17',
+            'name'   => '<br /> Plugin',
+            'desc'   => 'Enables simple newlines',
+            'url'    => 'http://flyspray.org/',
+        );
+    }
+
+    /**
+     * What kind of syntax are we?
+     */
+    function getType(){
+        return 'substition';
+    }
+
+    /**
+     * Where to sort in?
+     */
+    function getSort(){
+        return 201;
+    }
+
+    /**
+     * Connect pattern to lexer
+     */
+
+    function connectTo($mode) {
+        // Word boundaries?
+        $this->Lexer->addSpecialPattern("[^(\r\n)]+\r\n[^(\r\n)]+",$mode,'plugin_newline');
+        $this->Lexer->addSpecialPattern("[^\n]+\n[^\n]+",$mode,'plugin_newline');
+    }
+
+    /**
+     * Handle the match
+     */
+    function handle($match, $state, $pos, &$handler){
+        return array($match, $state);
+    }
+
+    /**
+     * Create output
+     */
+    function render($mode, &$renderer, $data) {
+        if($mode == 'xhtml'){
+            $renderer->doc .= str_replace("\n", '<br />', str_replace("\r\n", '<br />', $data[0]));
+        }
+        return true;
+    }
+
+}
+
+//Setup VIM: ex: et ts=4 enc=utf-8 :
+?>
