91913ec91e1247c7fe981d728b4c697c5120d8d2
lee <galactic.void@gmail.com>
2014-07-18 11:43:07 +0100

    array_intersect_key available since 5.1

--- includes/fix.inc.php
+++ includes/fix.inc.php
@@ -96,55 +96,6 @@ if (!isset($_SERVER['QUERY_STRING']))
     $_SERVER['QUERY_STRING'] = '';
 }
 
-/**
- * Replace array_intersect_key()
- *
- * @category    PHP
- * @package     PHP_Compat
- * @link        http://php.net/function.array_intersect_key
- * @author      Tom Buskens <ortega@php.net>
- * @version     $Revision: 1.8 $
- * @since       PHP 5.0.2
- * @require     PHP 4.0.0 (user_error)
- */
-function php_compat_array_intersect_key()
-{
-    $args = func_get_args();
-    $array_count = count($args);
-    if ($array_count < 2) {
-        user_error('Wrong parameter count for array_intersect_key()', E_USER_WARNING);
-        return;
-    }
-
-    // Check arrays
-    for ($i = $array_count; $i--;) {
-        if (!is_array($args[$i])) {
-            user_error('array_intersect_key() Argument #' .
-                ($i + 1) . ' is not an array', E_USER_WARNING);
-            return;
-        }
-    }
-
-    // Intersect keys
-    $arg_keys = array_map('array_keys', $args);
-    $result_keys = call_user_func_array('array_intersect', $arg_keys);
-
-    // Build return array
-    $result = array();
-    foreach($result_keys as $key) {
-        $result[$key] = $args[0][$key];
-    }
-    return $result;
-}
-
-// Define
-if (!function_exists('array_intersect_key')) {
-    function array_intersect_key()
-    {
-        $args = func_get_args();
-        return call_user_func_array('php_compat_array_intersect_key', $args);
-    }
-}
 
 /**
  * Replace glob() since this function is apparently
