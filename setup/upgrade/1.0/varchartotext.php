4ba197e75b1843dfe6e57700001ef3361f9ace9f
Jouni Ahto <jahto@welho.com>
2015-03-11 12:09:21 +0200

    Different syntax for PostgreSQL.

--- setup/upgrade/1.0/varchartotext.php
+++ setup/upgrade/1.0/varchartotext.php
@@ -1,5 +1,9 @@
 <?php
-
-$db->Query('ALTER TABLE {prefs} CHANGE `pref_value` `pref_value` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL');
-
+if ($conf['database']['dbtype'] == 'pgsql') {
+    $db->Query('ALTER TABLE {prefs} ALTER COLUMN pref_value TYPE text');
+    $db->Query('ALTER TABLE {prefs} ALTER COLUMN pref_value SET DEFAULT \'\'');    
+}
+else {
+    $db->Query('ALTER TABLE {prefs} CHANGE `pref_value` `pref_value` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL');
+}
 ?>
