ec832ee0929fa802721195228d248e6fd5e44782
peterdd <ddpm@liscovius.de>
2015-03-12 06:03:17 +0100

    localize upgrader: en

--- setup/lang/en.php
+++ setup/lang/en.php
@@ -1,5 +1,12 @@
 <?php
 $language=array(
-'performupgrade' =>'Perform Upgrade'
+'performupgrade'     => 'Perform Upgrade',
+'precautions'        => 'Precautions',
+'precautionbackup'   => 'Create a backup of your database and all Flyspray related files before performing the upgrade.',
+'preconditionchecks' => 'Precondition checks',
+'upgrade'            => 'Upgrade',
+'upgradepossible'    => 'Apparently, an upgrade is possible.',
+'versioncompare'     => 'Your current version is %s and the version we can upgrade to is %s.',
+'writeaccessconf'    => 'In order to upgrade Flyspray correctly it needs to be able to access and write flyspray.conf.php.',
 );
 ?>
