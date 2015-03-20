032387928122a0303ee7eee35352c6a649713981
peterdd <ddpm@liscovius.de>
2015-03-20 19:42:26 +0100

    send text and html in one mail
    i change directly to master, will revert my pull requests from install-hack branch.

--- includes/class.notify.php
+++ includes/class.notify.php
@@ -377,24 +377,21 @@ class Notifications {
         }
 
         // Make plaintext URLs into hyperlinks, but don't disturb existing ones!
-        $body = preg_replace("/(?<!\")(https?:\/\/)([a-zA-Z0-9\-.]+\.[a-zA-Z0-9\-]+([\/]([a-zA-Z0-9_\/\-.?&%=+#])*)*)/", '<a href="$1$2">$2</a>', $body);
-
-        // Make newlines into HTML line breaks
-        $body = str_replace("\n","<br>",$body);
-
+        $htmlbody = preg_replace("/(?<!\")(https?:\/\/)([a-zA-Z0-9\-.]+\.[a-zA-Z0-9\-]+([\/]([a-zA-Z0-9_\/\-.?&%=+#])*)*)/", '<a href="$1$2">$2</a>', $body);
+        $htmlbody = str_replace("\n","<br>", $htmlbody);
+        $plainbody= html_entity_decode(strip_tags($body), ENT_COMPAT | ENT_HTML401, 'utf-8');
+        
         $swift = Swift_Mailer::newInstance($swiftconn);
 
         $message = new Swift_Message($subject);
         if (isset($fs->prefs['emailNoHTML']) && $fs->prefs['emailNoHTML'] == '1'){
-            $body=html_entity_decode(strip_tags($body), ENT_COMPAT | ENT_HTML401, 'utf-8');
+           $message->setBody($plainbody, 'text/plain');
+        }else{
+           $message->setBody($htmlbody, 'text/html');
+           $message->addPart($plainbody, 'text/plain');
         }
-        $message->setBody($body);
+        
         $type = $message->getHeaders()->get('Content-Type');
-        if (isset($fs->prefs['emailNoHTML']) && $fs->prefs['emailNoHTML'] == '1'){
-            $type->setValue('text/plain');
-        } else {
-            $type->setValue('text/html');
-        }
         $type->setParameter('charset', 'utf-8');
 
         $message->getHeaders()->addTextHeader('Precedence', 'list');
