6ca6fae393d308d0105441138bc7ae64bf8dd928
peterdd <ddpm@liscovius.de>
2015-04-29 00:34:35 +0200

    Update en.php

--- lang/en.php
+++ lang/en.php
@@ -1017,6 +1017,7 @@ Regards,',
 'commentedon'             => 'commented on',
 'maxvoteperday'           => 'Maximum votes per day',
 'votesperproject'         => 'User\'s limit of votes per project',
-'votelimitreached'        => 'You reached your vote limit for this project. See your profile page for which tasks you currently vote. There you can also take back votes. So we can see what tasks are most important for you. Solved tasks get your vote back into your available voting limit.'
+'votelimitreached'        => 'You reached your vote limit for this project. See your profile page for which tasks you currently vote. There you can also take back votes. So we can see what tasks are most important for you. Solved tasks get your vote back into your available voting limit.',
+'myvotes'                 => 'My Votes',
 );
 ?>
