047f9e0e59ce80af4dc78bc789fc06a3906fa203
Jouni Ahto <jahto@welho.com>
2015-07-08 12:43:18 +0300

    Filter out unnecessary duplicates in GROUP BY clause.

--- includes/class.backend.php
+++ includes/class.backend.php
@@ -1627,7 +1627,7 @@ LEFT JOIN {users} u ON ass.user_id = u.user_id ';
         
         // echo '<pre>' . print_r($args, true) . '</pre>';
         // echo '<pre>' . print_r($cgroupbyarr, true) . '</pre>';
-        $cgroupby = count($cgroupbyarr) ? 'GROUP BY ' . implode(',', $cgroupbyarr) : '';
+        $cgroupby = count($cgroupbyarr) ? 'GROUP BY ' . implode(',', array_unique($cgroupbyarr)) : '';
 
         $sqlcount = "SELECT  COUNT(*) FROM (SELECT 1, t.task_id, t.date_opened, t.date_closed, t.last_edited_time
                            FROM     $cfrom
