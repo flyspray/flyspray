4f1376cbeb4a3930cf8f70d1fa5d3b9fe16bf496
peterdd <ddpm@liscovius.de>
2015-07-20 04:59:25 +0200

    token checks for deletesearches

--- js/callbacks/deletesearches.php
+++ js/callbacks/deletesearches.php
@@ -11,6 +11,17 @@ $baseurl = dirname(dirname($baseurl)) .'/' ;
 if (Cookie::has('flyspray_userid') && Cookie::has('flyspray_passhash')) {
     $user = new User(Cookie::val('flyspray_userid'));
     $user->check_account_ok();
+    
+    if( !Post::has('csrftoken') ){
+        header(':', true, 428); # 'Precondition Required'
+        die('missingtoken');
+    }elseif( Post::val('csrftoken')==$_SESSION['csrftoken']){
+        # empty
+    }else{
+        header(':', true, 412); # 'Precondition Failed'
+        die('wrongtoken');
+    }
+
     if (!$user->isAnon()) {
         $db->Query('DELETE FROM {searches} WHERE id = ? AND user_id = ?', array(Get::num('id'), $user->id));
     }
