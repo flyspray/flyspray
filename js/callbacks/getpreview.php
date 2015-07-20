68ac24f13b5ca31387303be2be4ceb5daaf42371
peterdd <ddpm@liscovius.de>
2015-07-20 06:27:18 +0200

    Update getpreview.php

--- js/callbacks/getpreview.php
+++ js/callbacks/getpreview.php
@@ -1,9 +1,4 @@
 <?php
-/*
-    This script is the AJAX callback that performs a search
-    for users, and returns them in an ordered list.
-*/
-
 define('IN_FS', true);
 
 header('Content-type: text/html; charset=utf-8');
@@ -14,10 +9,12 @@ require_once('../../header.php');
 if (Cookie::has('flyspray_userid') && Cookie::has('flyspray_passhash')) {
     $user = new User(Cookie::val('flyspray_userid'));
     $user->check_account_ok();
-    $user->save_search();
 } else {
     $user = new User(0, $proj);
 }
+
+# TODO csrftoken checking
+
 
 echo TextFormatter::render(Post::val('text'));
 
