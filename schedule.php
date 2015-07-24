93c7d45a6144f402cdf655150b447ab400813af0
Jouni Ahto <jahto@welho.com>
2015-07-24 07:56:45 +0300

    There was no need for that looping.

--- schedule.php
+++ schedule.php
@@ -26,9 +26,10 @@ function send_reminders() {
     $notify = & new Notifications;
     $user = & new User(0);
     $now = time();
-    $languages = array(); // Is this the right place? Move inside while loop if it's not...
+    $lang = $fs->prefs['lang_code'];
+    // $languages = array(); // Is this the right place? Move inside while loop if it's not...
 
-    $get_reminders = $db->Query("SELECT  r.*, t.*, p.*
+    $get_reminders = $db->Query("SELECT  r.*, t.*, u.*
                                FROM  {reminders} r
                          INNER JOIN  {users}     u ON u.user_id = r.to_user_id
                          INNER JOIN  {tasks}     t ON r.task_id = t.task_id
@@ -39,7 +40,7 @@ function send_reminders() {
     );
 
     while ($row = $db->FetchRow($get_reminders)) {
-        // So that the sender in emails will be the right project, not 'Default project'
+        // So that the sender in emails will is the right project, not 'Default project'
         // and also to get the projects default language, if needed.
         $proj = new Project($row['project_id']);
         $jabber_users = array();
@@ -50,59 +51,60 @@ function send_reminders() {
         $emails_to_send = array();
 
         // Get the user's notification type, address, desired message and preferred language
-        $get_details = $db->Query("SELECT  notify_type, jabber_id, email_address, lang_code
-                                 FROM  {users}
-                                WHERE  user_id = ?", array($row['to_user_id']));
-
-        while ($subrow = $db->FetchRow($get_details)) {
-            if (($fs->prefs['user_notify'] == '1' && $subrow['notify_type'] == '1')
-                    OR ( $fs->prefs['user_notify'] == '2')) {
-                // $email_users[] = $subrow['email_address'];
-                $email_users[] = array('recipient' => $subrow['email_address'],
-                    'lang' => $subrow['lang_code'],
-                    'message' => $row['reminder_message']);
-            } elseif (($fs->prefs['user_notify'] == '1' && $subrow['notify_type'] == '2')
-                    OR ( $fs->prefs['user_notify'] == '3')) {
-                // $jabber_users[] = $subrow['jabber_id'];
-                $jabber_users[] = array('recipient' => $subrow['email_address'],
-                    'lang' => $subrow['lang_code'],
-                    'message' => $row['reminder_message']);
-            }
+        /* Details now already fetched in the previous query. 
+          $get_details = $db->Query("SELECT  notify_type, jabber_id, email_address, lang_code
+          FROM  {users}
+          WHERE  user_id = ?", array($row['to_user_id']));
+         */
+        // while ($subrow = $db->FetchRow($get_details)) {
+        if (($fs->prefs['user_notify'] == '1' && $row['notify_type'] == '1')
+                OR ( $fs->prefs['user_notify'] == '2')) {
+            $email_users[] = $row['email_address'];
+        } elseif (($fs->prefs['user_notify'] == '1' && $row['notify_type'] == '2')
+                OR ( $fs->prefs['user_notify'] == '3')) {
+            $jabber_users[] = $row['jabber_id'];
         }
+        // }
 
-        // Ok, we've got the recipients collected with their preferred messages and preferred languages.
-        // Next, sort them by language so we get also the message subject right. Send emails immediately
-        // while the project is still the right one to get also the sender right, just store jabber messages.
-        foreach ($email_users as $recipient) {
-            if (!empty($recipient['lang'])) {
-                $lang = $recipient['lang'];
-            } else if (!empty($proj->prefs['lang_code'])) {
-                $lang = $proj->prefs['lang_code'];
-            } else {
-                $lang = $fs->prefs['lang_code'];
-            }
-            $emails[$lang][] = $recipient;
-            if (!in_array($lang, $languages)) {
-                $languages[] = $lang;
-            }
+        if (!empty($row['lang_code'])) {
+            $lang = $row['lang_code'];
+        } else if (!empty($proj->prefs['lang_code'])) {
+            $lang = $proj->prefs['lang_code'];
+        } else {
+            $lang = $fs->prefs['lang_code'];
         }
+        /*
+          foreach ($email_users as $recipient) {
+          if (!empty($recipient['lang'])) {
+          $lang = $recipient['lang'];
+          } else if (!empty($proj->prefs['lang_code'])) {
+          $lang = $proj->prefs['lang_code'];
+          } else {
+          $lang = $fs->prefs['lang_code'];
+          }
+          $emails[$lang][] = $recipient;
+          if (!in_array($lang, $languages)) {
+          $languages[] = $lang;
+          }
+          }
 
-        foreach ($jabber_users as $recipient) {
-            if (!empty($recipient['lang'])) {
-                $lang = $recipient['lang'];
-            } else if (!empty($proj->prefs['lang_code'])) {
-                $lang = $proj->prefs['lang_code'];
-            } else {
-                $lang = $fs->prefs['lang_code'];
-            }
-            $jabbers[$lang][] = $recipient;
-            if (!in_array($lang, $languages)) {
-                $languages[] = $lang;
-            }
-        }
-
+          foreach ($jabber_users as $recipient) {
+          if (!empty($recipient['lang'])) {
+          $lang = $recipient['lang'];
+          } else if (!empty($proj->prefs['lang_code'])) {
+          $lang = $proj->prefs['lang_code'];
+          } else {
+          $lang = $fs->prefs['lang_code'];
+          }
+          $jabbers[$lang][] = $recipient;
+          if (!in_array($lang, $languages)) {
+          $languages[] = $lang;
+          }
+          }
+         */
         // Now, loop trough the possible languages. Oh fuck, I forgot that the message also could
         // be customized for each individual recipient. Have to look at that one later (or now).
+        /*
         foreach ($languages as $lang) {
             $subject = tL('notifyfromfs', $lang);
             if (isset($emails[$lang])) {
@@ -122,14 +124,21 @@ function send_reminders() {
                 }
             }
         }
-        /*
-        $subject = L('notifyfromfs');
+        */
+        $subject = tL('notifyfromfs', $lang);
         $message = $row['reminder_message'];
-
+        
         // Pass the recipients and message onto the notification function
         $notify->SendEmail($email_users, $subject, $message);
         $notify->StoreJabber($jabber_users, $subject, $message);
-        */
+        /*
+          $subject = L('notifyfromfs');
+          $message = $row['reminder_message'];
+
+          // Pass the recipients and message onto the notification function
+          $notify->SendEmail($email_users, $subject, $message);
+          $notify->StoreJabber($jabber_users, $subject, $message);
+         */
         // Update the database with the time sent
         $update_db = $db->Query("UPDATE  {reminders}
                                SET  last_sent = ?
