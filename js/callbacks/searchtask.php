066f80462105ecbde7a61ae16f0da37b31a1040c
Landrok <landrok@users.noreply.github.com>
2015-09-28 20:38:21 +0200

    Fix params order
    Summary and details were inverted

--- js/callbacks/searchtask.php
+++ js/callbacks/searchtask.php
@@ -14,8 +14,7 @@ if(!Post::has('detail') || !Post::has('summary') || !Post::has('project_id'))
 if (Cookie::has('flyspray_userid') && Cookie::has('flyspray_passhash')){
   $user = new User(Cookie::val('flyspray_userid'));
   $user->check_account_ok();
-}
-else {
+} else {
   $user = new User(0, $proj);
 }
 
@@ -28,14 +27,16 @@ if(!$user->can_open_task($proj)){
 // Prepare SQL params
 $params = array(
   'project_id' => Post::num('project_id'),
-  'details' => "%" . trim(Post::val('detail')) . "%",
-  'summary' => "%" . trim(Post::val('summary')) . "%"
+  'summary' => "%" . trim(Post::val('summary')) . "%",
+  'details' => "%" . trim(Post::val('detail')) . "%"
 );
 
 $sql = $db->Query('SELECT count(*) 
-		     FROM {tasks} t
-		     WHERE t.project_id = ? AND t.item_summary = ? AND t.detailed_desc like ?',
-		     $params);
+		   FROM {tasks} t
+		   WHERE t.project_id = ? 
+		   	AND t.item_summary = ? 
+		   	AND t.detailed_desc like ?'
+		   $params);
 $sametask = $db->fetchOne($sql);
 echo $sametask;
 
