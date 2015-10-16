3d540fa39b1093b6a2d53be650b75af249ead2bb
peterdd <ddpm@liscovius.de>
2015-10-16 08:23:53 +0200

    Update modify.inc.php

--- includes/modify.inc.php
+++ includes/modify.inc.php
@@ -982,7 +982,8 @@ switch ($action = Req::val('action'))
 			    'smtp_user', 'smtp_pass', 'funky_urls', 'reminder_daemon','cache_feeds', 'intro_message',
                 'disable_lostpw','disable_changepw','days_before_alert', 'emailNoHTML', 'need_approval', 'pages_welcome_msg',
                 'active_oauths', 'only_oauth_reg', 'enable_avatars', 'max_avatar_size', 'default_order_by', 'default_order_by_dir',
-                'max_vote_per_day', 'votes_per_project', 'url_rewriting');
+                'max_vote_per_day', 'votes_per_project', 'url_rewriting',
+                'custom_style');
         if(Post::val('need_approval') == '1' && Post::val('spam_proof'))
             unset($_POST['spam_proof']);//if self register request admin to approve, disable spam_proof
         //if you think different, modify functions in class.user.php directing different regiser tpl
