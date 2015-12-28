eae84c873626ab863e438fc09abfab703cfa7cc2
peterdd <ddpm@liscovius.de>
2015-12-28 13:40:21 +0100

    Update bootstrap.php

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,10 +1,17 @@
 <?php
-function loader($class)
-{
-$file = $class . '.php';
-if (file_exists($file)) {
-require $file;
+/*
+function loader($class) {
+  $file = $class . '.php';
+  if (file_exists($file)) {
+    require $file;
+  }
 }
-}
+
 spl_autoload_register('loader');
+*/
+
+if(is_readable('vendor/autoload.php')){
+  // Use composer autoloader
+  require 'vendor/autoload.php';
+}
 ?>
