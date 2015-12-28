8fcba8ba4abeae509da39f911754ddb95416b88a
peterdd <ddpm@liscovius.de>
2015-12-28 13:34:48 +0100

    Update flysprayTest.php

--- Tests/flysprayTest.php
+++ Tests/flysprayTest.php
@@ -8,13 +8,13 @@ class FlysprayTest extends PHPUnit_Framework_TestCase{
     #$this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
     #$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     #$this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
-    this->db = new Database;
-    this->db->dbOpen($GLOBALS['dbhost'], $GLOBALS['dbuser'], $GLOBALS['dbpass'], $GLOBALS['dbname'], $GLOBALS['dbtype'], $GLOBALS['dbprefix']);
-    this->db->Query("CREATE TABLE {projects} (what VARCHAR(50) NOT NULL)");
+    $this->db = new Database;
+    $this->db->dbOpen($GLOBALS['dbhost'], $GLOBALS['dbuser'], $GLOBALS['dbpass'], $GLOBALS['dbname'], $GLOBALS['dbtype'], $GLOBALS['dbprefix']);
+    $this->db->Query("CREATE TABLE {projects} (what VARCHAR(50) NOT NULL)");
   }
   public function tearDown(){
     #$this->pdo->query("DROP TABLE hello");
-    this->db->Query("DROP TABLE {projects}");
+    $this->db->Query("DROP TABLE {projects}");
   }
   
   public function testHelloWorld(){
