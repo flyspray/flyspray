eaf673749483e106cdafd5661541206bc7500968
peterdd <ddpm@liscovius.de>
2015-12-28 14:16:42 +0100

    Update bootstrap.php

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -10,6 +10,8 @@ function loader($class) {
 spl_autoload_register('loader');
 */
 
+define('IN_FS', true); # for passing some checks in Flyspray files
+
 if(is_readable('vendor/autoload.php')){
   // Use composer autoloader
   require 'vendor/autoload.php';
