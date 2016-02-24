9929f3c50d0d6611c56b57b3590d84612929ec6b
peterdd <ddpm@liscovius.de>
2016-02-24 09:08:51 +0100

    token check for save search

--- js/callbacks/savesearches.php
+++ js/callbacks/savesearches.php
@@ -10,6 +10,17 @@ require_once('../../header.php');
 if (Cookie::has('flyspray_userid') && Cookie::has('flyspray_passhash')) {
     $user = new User(Cookie::val('flyspray_userid'));
     $user->check_account_ok();
+
+    if( !Post::has('csrftoken') ){
+        header(':', true, 428); # 'Precondition Required'
+        die('missingtoken');
+    }elseif( Post::val('csrftoken')==$_SESSION['csrftoken']){
+        # empty
+    }else{
+        header(':', true, 412); # 'Precondition Failed'
+        die('wrongtoken');
+    }
+    
     $user->save_search();
 }
 
