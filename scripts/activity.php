74e253e38ae3b3b226943eae78ae7ac4cf7ca057
peterdd <ddpm@liscovius.de>
2016-10-12 04:29:04 +0200

    Update activity.php
    small logic simplification

--- scripts/activity.php
+++ scripts/activity.php
@@ -1,28 +1,25 @@
 <?php
-/*******************************\
-| Activity Graph Maker 					|
-| Renders a graph for topview   |
-\*******************************/
+/*****************************\
+| Activity Graph Maker        |
+| Renders a graph for topview |
+\*****************************/
 
 if (!defined('IN_FS')) {
     die('Do not access this file directly.');
 }
 
+$data='';
+
 # Project Graph
 if ((Get::has('project_id') && Get::val('graph', 'project') == 'project')) {
-	if (!$user->can_view_project(Get::num('project_id'))) {
-		# empty data
-		$data = '';
-		# and make the zero-line 'invisible'
-		$_GET['line']='fff';
-	} else{
+	if ($user->can_view_project(Get::num('project_id'))) {
 		$today          = date('Y-m-d');
 		$thirtyone_days = date('U' , strtotime("-31 day", strtotime($today)));
 		$sixtyone_days  = date('U' , strtotime("-61 day", strtotime($today)));
-    
+
 		//look 30 + days and if found scale
 		$projectCheck = Project::getActivityProjectCount($sixtyone_days, $thirtyone_days, Get::num('project_id'));
-    
+
 		if($projectCheck > 0) {
 			$data = Project::getDayActivityByProject($sixtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'));
 		} else {
@@ -30,22 +27,20 @@ if ((Get::has('project_id') && Get::val('graph', 'project') == 'project')) {
 		}
 
 		$data = implode(',', $data);
-	}    
-# User Graph
-} else if(Get::has('user_id') && Get::has('project_id') && Get::val('graph') == 'user') {
-	if (!$user->can_view_project(Get::num('project_id'))) {
-		# empty data
-		$data = '';
+	} else {
 		# and make the zero-line 'invisible'
 		$_GET['line']='fff';
-	} else{
+	}   
+# User Graph
+} else if(Get::has('user_id') && Get::has('project_id') && Get::val('graph') == 'user') {
+	if ($user->can_view_project(Get::num('project_id'))) {
 		$today          = date('Y-m-d');
 		$thirtyone_days = date('U' , strtotime("-31 day", strtotime($today)));
 		$sixtyone_days  = date('U' , strtotime("-61 day", strtotime($today)));
-    
+
 		//look 30 + days and if found scale
 		$projectCheck = Project::getActivityProjectCount($sixtyone_days, $thirtyone_days, Get::num('project_id'));
-    
+
 		if($projectCheck > 0) {
 			$data = User::getDayActivityByUser($sixtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'), Get::num('user_id'));
 		} else {
@@ -53,9 +48,12 @@ if ((Get::has('project_id') && Get::val('graph', 'project') == 'project')) {
 		}
 
 		$data = implode(',', $data);
-	}
+	} else {
+		# and make the zero-line 'invisible'
+		$_GET['line']='fff';
+	} 
 } else {
-	$data = '';
+	# make the zero-line 'invisible'
 	$_GET['line']='fff';
 }
 
