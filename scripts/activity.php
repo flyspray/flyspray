05a9fe2cae8bb93d2c8f1ddd3079d86aef02868d
peterdd <ddpm@liscovius.de>
2016-10-12 04:42:38 +0200

    permission checks for activity line graphs (#590)
    security fix: The activity line-graph feature didn't check any project permission.
    This adds the missing project permission checks.

--- scripts/activity.php
+++ scripts/activity.php
@@ -1,50 +1,60 @@
 <?php
-
-  /********************************************************\
-  | Activity Graph Maker 								   |
-  | Renders a graph for topview                            |
-  | ~~~~~~~~~~~~~                                          |
-  \********************************************************/
+/*****************************\
+| Activity Graph Maker        |
+| Renders a graph for topview |
+\*****************************/
 
 if (!defined('IN_FS')) {
     die('Do not access this file directly.');
 }
 
-//PROJECT GRAPH
+$data='';
+
+# Project Graph
 if ((Get::has('project_id') && Get::val('graph', 'project') == 'project')) {
-    $today          = date('Y-m-d');
-    $thirtyone_days = date('U' , strtotime("-31 day", strtotime($today)));
-    $sixtyone_days  = date('U' , strtotime("-61 day", strtotime($today)));
-    
-    //look 30 + days and if found scale
-    $projectCheck = Project::getActivityProjectCount($sixtyone_days, $thirtyone_days, Get::num('project_id'));
-    
-    if($projectCheck > 0) {
-        $data = Project::getDayActivityByProject($sixtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'));
-    } else {
-        $data = Project::getDayActivityByProject($thirtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'));
-    }
-    
-    $data = implode(',', $data);
-    
-//User Graph
+	if ($user->can_view_project(Get::num('project_id'))) {
+		$today          = date('Y-m-d');
+		$thirtyone_days = date('U' , strtotime("-31 day", strtotime($today)));
+		$sixtyone_days  = date('U' , strtotime("-61 day", strtotime($today)));
+
+		//look 30 + days and if found scale
+		$projectCheck = Project::getActivityProjectCount($sixtyone_days, $thirtyone_days, Get::num('project_id'));
+
+		if($projectCheck > 0) {
+			$data = Project::getDayActivityByProject($sixtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'));
+		} else {
+			$data = Project::getDayActivityByProject($thirtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'));
+		}
+
+		$data = implode(',', $data);
+	} else {
+		# and make the zero-line 'invisible'
+		$_GET['line']='fff';
+	}   
+# User Graph
 } else if(Get::has('user_id') && Get::has('project_id') && Get::val('graph') == 'user') {
-    $today          = date('Y-m-d');
-    $thirtyone_days = date('U' , strtotime("-31 day", strtotime($today)));
-    $sixtyone_days  = date('U' , strtotime("-61 day", strtotime($today)));
-    
-    //look 30 + days and if found scale
-    $projectCheck = Project::getActivityProjectCount($sixtyone_days, $thirtyone_days, Get::num('project_id'));
-    
-    if($projectCheck > 0) {
-        $data = User::getDayActivityByUser($sixtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'), Get::num('user_id'));
-    } else {
-        $data = User::getDayActivityByUser($thirtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'), Get::num('user_id'));
-    }
-    
-    $data = implode(',', $data);
+	if ($user->can_view_project(Get::num('project_id'))) {
+		$today          = date('Y-m-d');
+		$thirtyone_days = date('U' , strtotime("-31 day", strtotime($today)));
+		$sixtyone_days  = date('U' , strtotime("-61 day", strtotime($today)));
+
+		//look 30 + days and if found scale
+		$projectCheck = Project::getActivityProjectCount($sixtyone_days, $thirtyone_days, Get::num('project_id'));
+
+		if($projectCheck > 0) {
+			$data = User::getDayActivityByUser($sixtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'), Get::num('user_id'));
+		} else {
+			$data = User::getDayActivityByUser($thirtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'), Get::num('user_id'));
+		}
+
+		$data = implode(',', $data);
+	} else {
+		# and make the zero-line 'invisible'
+		$_GET['line']='fff';
+	} 
 } else {
-    $data = '';
+	# make the zero-line 'invisible'
+	$_GET['line']='fff';
 }
 
 // Not pretty but gets the job done.
