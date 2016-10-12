26d8ca625c15e781fb775e9e2564f3613c2b1617
peterdd <ddpm@liscovius.de>
2016-10-12 03:56:21 +0200

    add project permission checks to activity graphs

--- scripts/activity.php
+++ scripts/activity.php
@@ -1,50 +1,62 @@
 <?php
-
-  /********************************************************\
-  | Activity Graph Maker 								   |
-  | Renders a graph for topview                            |
-  | ~~~~~~~~~~~~~                                          |
-  \********************************************************/
+/*******************************\
+| Activity Graph Maker 					|
+| Renders a graph for topview   |
+\*******************************/
 
 if (!defined('IN_FS')) {
     die('Do not access this file directly.');
 }
 
-//PROJECT GRAPH
+# Project Graph
 if ((Get::has('project_id') && Get::val('graph', 'project') == 'project')) {
-    $today          = date('Y-m-d');
-    $thirtyone_days = date('U' , strtotime("-31 day", strtotime($today)));
-    $sixtyone_days  = date('U' , strtotime("-61 day", strtotime($today)));
+	if (!$user->can_view_project(Get::num('project_id'))) {
+		# empty data
+		$data = '';
+		# and make the zero-line 'invisible'
+		$_GET['line']='fff';
+	} else{
+		$today          = date('Y-m-d');
+		$thirtyone_days = date('U' , strtotime("-31 day", strtotime($today)));
+		$sixtyone_days  = date('U' , strtotime("-61 day", strtotime($today)));
     
-    //look 30 + days and if found scale
-    $projectCheck = Project::getActivityProjectCount($sixtyone_days, $thirtyone_days, Get::num('project_id'));
+		//look 30 + days and if found scale
+		$projectCheck = Project::getActivityProjectCount($sixtyone_days, $thirtyone_days, Get::num('project_id'));
     
-    if($projectCheck > 0) {
-        $data = Project::getDayActivityByProject($sixtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'));
-    } else {
-        $data = Project::getDayActivityByProject($thirtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'));
-    }
-    
-    $data = implode(',', $data);
-    
-//User Graph
+		if($projectCheck > 0) {
+			$data = Project::getDayActivityByProject($sixtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'));
+		} else {
+			$data = Project::getDayActivityByProject($thirtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'));
+		}
+
+		$data = implode(',', $data);
+	}    
+# User Graph
 } else if(Get::has('user_id') && Get::has('project_id') && Get::val('graph') == 'user') {
-    $today          = date('Y-m-d');
-    $thirtyone_days = date('U' , strtotime("-31 day", strtotime($today)));
-    $sixtyone_days  = date('U' , strtotime("-61 day", strtotime($today)));
+	if (!$user->can_view_project(Get::num('project_id'))) {
+		# empty data
+		$data = '';
+		# and make the zero-line 'invisible'
+		$_GET['line']='fff';
+	} else{
+		$today          = date('Y-m-d');
+		$thirtyone_days = date('U' , strtotime("-31 day", strtotime($today)));
+		$sixtyone_days  = date('U' , strtotime("-61 day", strtotime($today)));
     
-    //look 30 + days and if found scale
-    $projectCheck = Project::getActivityProjectCount($sixtyone_days, $thirtyone_days, Get::num('project_id'));
+		//look 30 + days and if found scale
+		$projectCheck = Project::getActivityProjectCount($sixtyone_days, $thirtyone_days, Get::num('project_id'));
     
-    if($projectCheck > 0) {
-        $data = User::getDayActivityByUser($sixtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'), Get::num('user_id'));
-    } else {
-        $data = User::getDayActivityByUser($thirtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'), Get::num('user_id'));
-    }
-    
-    $data = implode(',', $data);
+		if($projectCheck > 0) {
+			$data = User::getDayActivityByUser($sixtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'), Get::num('user_id'));
+		} else {
+			$data = User::getDayActivityByUser($thirtyone_days, date('U', strtotime(date('Y-m-d'))), Get::num('project_id'), Get::num('user_id'));
+		}
+
+		$data = implode(',', $data);
+	}
 } else {
-    $data = '';
+	$data = '';
+	$_GET['line']='fff';
 }
 
 // Not pretty but gets the job done.
