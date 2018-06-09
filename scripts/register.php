fa4b970553de3c5d6538b4be81ce3ade9066fe9a
peterdd <ddpm@liscovius.de>
2018-06-09 02:48:32 +0200

    Update register.php

--- scripts/register.php
+++ scripts/register.php
@@ -12,17 +12,17 @@ if (!defined('IN_FS')) {
 $page->setTitle($fs->prefs['page_title'] . L('registernewuser'));
 
 if (!$user->isAnon()) {
-    Flyspray::Redirect($baseurl);
+    Flyspray::redirect($baseurl);
 }
 
 if ($user->can_register()) {
     // 32 is the length of the magic_url
     if (Req::has('magic_url') && strlen(Req::val('magic_url')) == 32) {
         // If the user came here from their notification link
-        $sql = $db->Query('SELECT * FROM {registrations} WHERE magic_url = ?',
+        $sql = $db->query('SELECT * FROM {registrations} WHERE magic_url = ?',
                           array(Get::val('magic_url')));
 
-        if (!$db->CountRows($sql)) {
+        if (!$db->countRows($sql)) {
             Flyspray::show_error(18);
         }
 
