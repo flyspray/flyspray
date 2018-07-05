503d22f9897324aeaf85852ef72edf470af5e714
peterdd <ddpm@liscovius.de>
2018-07-05 05:12:43 +0200

    fallback hash_equals for  php<php5.6

--- includes/fix.inc.php
+++ includes/fix.inc.php
@@ -58,6 +58,21 @@ if(!function_exists('password_hash')){
 	require_once dirname(__FILE__).'/password_compat.php';
 }
 
+# for php < php.5.6
+if(!function_exists('hash_equals')) {
+	function hash_equals($str1, $str2) {
+		if(strlen($str1) != strlen($str2)) {
+			return false;
+		} else {
+			$res = $str1 ^ $str2;
+			$ret = 0;
+			for($i = strlen($res) - 1; $i >= 0; $i--) $ret |= ord($res[$i]);
+			return !$ret;
+		}
+	}
+}
+
+
 ini_set('include_path', join( PATH_SEPARATOR, array(
   dirname(__FILE__) . '/external' ,
   ini_get('include_path'))));
