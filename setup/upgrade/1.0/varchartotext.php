e06905dfe92652d38c108e86d5dae1559f3cd3b5
peterdd <ddpm@liscovius.de>
2018-12-18 15:12:58 +0100

    lowercase start function calls..

--- setup/upgrade/1.0/varchartotext.php
+++ setup/upgrade/1.0/varchartotext.php
@@ -1,17 +1,17 @@
 <?php
 if ($conf['database']['dbtype'] == 'pgsql') {
-    $db->Query('ALTER TABLE {prefs} ALTER COLUMN pref_value TYPE text');
-    $db->Query('ALTER TABLE {prefs} ALTER COLUMN pref_value SET DEFAULT \'\'');    
+    $db->query('ALTER TABLE {prefs} ALTER COLUMN pref_value TYPE text');
+    $db->query('ALTER TABLE {prefs} ALTER COLUMN pref_value SET DEFAULT \'\'');    
 }
 elseif($db->dbtype=='mysqli' || $db->dbtype=='mysql') {
-    $sinfo=$db->dblink->ServerInfo();
+    $sinfo=$db->dblink->serverInfo();
     if(isset($sinfo['version']) && version_compare($sinfo['version'], '5.5.3')>=0 ){
-        $db->Query('ALTER TABLE {prefs} CHANGE `pref_value` `pref_value` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL');
+        $db->query('ALTER TABLE {prefs} CHANGE `pref_value` `pref_value` TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL');
     }else{
-        $db->Query('ALTER TABLE {prefs} CHANGE `pref_value` `pref_value` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL');
+        $db->query('ALTER TABLE {prefs} CHANGE `pref_value` `pref_value` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL');
     }
 }
 else{
-    $db->Query('ALTER TABLE {prefs} CHANGE `pref_value` `pref_value` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL');   
+    $db->query('ALTER TABLE {prefs} CHANGE `pref_value` `pref_value` TEXT CHARACTER SET utf8 COLLATE utf8_bin NOT NULL');   
 }
 ?>
