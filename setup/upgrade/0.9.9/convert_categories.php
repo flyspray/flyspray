a48831bb3c97a7187639d2be6ecf6233ce168b83
peterdd <ddpm@liscovius.de>
2018-12-18 15:18:44 +0100

    lowercase start function calls..

--- setup/upgrade/0.9.9/convert_categories.php
+++ setup/upgrade/0.9.9/convert_categories.php
@@ -10,9 +10,9 @@ function rebuild_tree($parent, $left, $pr) {
    $right = $left+1;
 
    // get all children of this node
-   $result = $db->Query('SELECT category_id FROM {list_category} WHERE parent_id = ? AND project_id = ?', array($parent, $pr));
+   $result = $db->query('SELECT category_id FROM {list_category} WHERE parent_id = ? AND project_id = ?', array($parent, $pr));
 
-   while ($row = $db->FetchRow($result)) {
+   while ($row = $db->fetchRow($result)) {
        // recursive execution of this function for each
        // child of this node
        // $right is the current right value, which is
@@ -22,21 +22,21 @@ function rebuild_tree($parent, $left, $pr) {
 
    // we've got the left value, and now that we've processed
    // the children of this node we also know the right value
-   $db->Query('UPDATE {list_category} SET lft= ?, rgt= ? WHERE category_id = ?', array($left, $right, $parent));
-   $sql = $db->Query('SELECT * FROM {list_category} WHERE category_id = ? OR project_id=? AND parent_id=-1', array($parent, $pr));
-   if (!$db->CountRows($sql)) {
-       $db->Query('INSERT INTO {list_category} (project_id, lft, rgt, category_name, parent_id) VALUES(?,?,?,?,-1)',
+   $db->query('UPDATE {list_category} SET lft= ?, rgt= ? WHERE category_id = ?', array($left, $right, $parent));
+   $sql = $db->query('SELECT * FROM {list_category} WHERE category_id = ? OR project_id=? AND parent_id=-1', array($parent, $pr));
+   if (!$db->countRows($sql)) {
+       $db->query('INSERT INTO {list_category} (project_id, lft, rgt, category_name, parent_id) VALUES(?,?,?,?,-1)',
                   array($pr,$left,$right,'root'));
    }
    // return the right value of this node + 1
    return $right+1;
 } 
 
-$projects = $db->Query('SELECT project_id FROM {projects}');
+$projects = $db->query('SELECT project_id FROM {projects}');
 
 // Global project
 rebuild_tree(0, 1, 0);
-while ($pr = $db->FetchRow($projects)) {
+while ($pr = $db->fetchRow($projects)) {
     rebuild_tree(0, 1, $pr['project_id']);
 }
 
