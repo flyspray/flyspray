65e34460eabd6bd39c57b92f2a55b4392c677c57
peterdd <ddpm@liscovius.de>
2019-04-22 03:15:40 +0200

    Update flysprayTest.php

--- tests/flysprayTest.php
+++ tests/flysprayTest.php
@@ -7,7 +7,7 @@ class FlysprayTest extends TestCase{
   private $db;
   
   # just taken as first test from github project travis-ci-examples/php
-  public function setUp(){
+  protected function setUp(){
     #$this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
     #$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     #$this->pdo->query("CREATE TABLE hello (what VARCHAR(50) NOT NULL)");
