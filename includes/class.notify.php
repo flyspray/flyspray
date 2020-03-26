7a18a1c1055e61f80350478b573012abee96234b
peterdd <ddpm@liscovius.de>
2020-03-26 21:37:40 +0100

    fix FS#2596 port in notifications email links
    The automatic converting of urls to a-tags in the html email notification part missed the possibility of running Flyspray on a custom port.

--- includes/class.notify.php
+++ includes/class.notify.php
@@ -413,7 +413,7 @@ class Notifications {
         }
 
         // Make plaintext URLs into hyperlinks, but don't disturb existing ones!
-        $htmlbody = preg_replace("/(?<!\")(https?:\/\/)([a-zA-Z0-9\-.]+\.[a-zA-Z0-9\-]+([\/]([a-zA-Z0-9_\/\-.?&%=+#])*)*)/", '<a href="$1$2">$2</a>', $body);
+        $htmlbody = preg_replace("/(?<!\")(https?:\/\/)([a-z0-9\-.]+\.[a-z\-]+(:[0-9]+)?([\/]([a-z0-9_\/\-.?&%=+#])*)*)/i", '<a href="$1$2">$2</a>', $body);
         $htmlbody = str_replace("\n","<br>", $htmlbody);
         
         // Those constants used were introduced in 5.4.
