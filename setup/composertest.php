0c51febdb72b349dbd3132dea1e0d31c1dda4d40
peterdd <ddpm@liscovius.de>
2021-03-18 04:45:25 +0100

    typo

--- setup/composertest.php
+++ setup/composertest.php
@@ -29,7 +29,7 @@ header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
-
+?>
 <!DOCTYPE html>
 <html>
 <head>
