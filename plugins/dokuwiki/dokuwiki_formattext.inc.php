8d6e2a7400167523ccb0fd9d2863e90ad9a25818
peterdd <ddpm@liscovius.de>
2021-08-06 01:15:16 +0200

    Update dokuwiki_formattext.inc.php

--- plugins/dokuwiki/dokuwiki_formattext.inc.php
+++ plugins/dokuwiki/dokuwiki_formattext.inc.php
@@ -1,10 +1,10 @@
 <?php
 class dokuwiki_TextFormatter
-{    
+{
 	static function render($text, $type = null, $id = null, $instructions = null)
 	{
 		global $conf, $baseurl, $db, $fs;
-        
+
 		// Unfortunately dokuwiki also uses $conf
 		$fs_conf = $conf;
 		$conf = array();
@@ -22,7 +22,6 @@ class dokuwiki_TextFormatter
 
 		if (!is_string($instructions) || strlen($instructions) < 1) {
 			$modes = p_get_parsermodes();
-            
 			$Parser = new Doku_Parser();
 			// Add the Handler
 			$Parser->Handler = new Doku_Handler();
@@ -68,7 +67,7 @@ class dokuwiki_TextFormatter
 		}
 
 		$return = $Renderer->doc;
-		
+
 		// Display the output
 		if (Get::val('string')) {
 			$words = explode(' ', Get::val('string'));
@@ -76,7 +75,7 @@ class dokuwiki_TextFormatter
 				$return = html_hilight($return, $word);
 			}
 		}
-        
+
 		return $return;
 	}
 
