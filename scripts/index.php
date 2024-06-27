flyspray/flyspray

commit 5b85fc8f030021b254bebe211764020f95d53d47

Flyspray/Flyspray #887: Task List Table Header Sort Icons
Restored visibility of Attachment, Comments, and Votes icons which a
previous commit in FS_887_ui_improvements has broken.

Replace sort indicator images with Font Awesome Icons.

Wrapped colunn name text with span in order to selectively apply
margin-left to certain icons.

Primary sort icon is contained in the link as before.

Secondary sort icon (order2) added after the link as needed, colored
gray.  If Flyspray is going to carry forward the previous sort, the
user should have some indication that this is happening.

Sorted th elements now gain class 'orderby' or 'orderby2' as needed,
as well as class 'asc' or 'desc' to allow styling based on the current
state of the table.

scripts/index.php
@@ -158,31 +158,46 @@ function tpl_list_heading($colname, $format = "<th%s>%s</th>")
 {
     global $proj, $page;
     $imgbase = '<img src="%s" alt="%s" />';
+    $sorticonbase = '<span class="fas %s fa-lg"></span>';
     $class   = $colname;
-    $html    = eL($colname);
+    $html    = '<span>' . eL($colname) . '</span>';
+    $afterhtml = '';
 /*
     if ($colname == 'comments' || $colname == 'attachments') {
         $html = sprintf($imgbase, $page->get_image(substr($colname, 0, -1)), $html);
     }
 */
 	if ($colname == 'attachments') {
-		$html='<i class="fa fa-paperclip fa-lg" title="'.$html.'"></i>';
+		$html='<span class="fas fa-paperclip fa-lg" title="'.$html.'"></span>';
 	}
 	if ($colname == 'comments') {
-		$html='<i class="fa fa-comments fa-lg" title="'.$html.'"></i>';
+		$html='<span class="fas fa-comments fa-lg" title="'.$html.'"></span>';
 	}
 	if ($colname == 'votes') {
-		$html='<i class="fa fa-star-o fa-lg" title="'.$html.'"></i>';
+		$html='<span class="far fa-star fa-lg" title="'.$html.'"></span>';
 	}
 
     if (Get::val('order') == $colname) {
         $class .= ' orderby';
         $sort1  = Get::safe('sort', 'desc') == 'desc' ? 'asc' : 'desc';
+        $sort1now = ($sort1 == 'asc' ? 'desc' : 'asc');
+        $icon = 'fa-' . ($sort1 == 'asc' ? 'arrow-down-wide-short' : 'arrow-up-short-wide');
         $sort2  = Get::safe('sort2', 'desc');
         $order2 = Get::safe('order2');
-        $html  .= '&nbsp;&nbsp;'.sprintf($imgbase, $page->get_image(Get::val('sort')), Get::safe('sort'));
-    }
-    else {
+        $class .= " $sort1now";
+        $html .= sprintf($sorticonbase, $icon);
+    } else {
+        if (Get::val('order2') == $colname) {
+            $class .= ' orderby2';
+            $sort2  = Get::safe('sort2', 'desc') == 'desc' ? 'asc' : 'desc';
+            $sort2now = ($sort2 == 'asc' ? 'desc' : 'asc');
+            $order2 = Get::safe('order2');
+            $icon2 = 'fa-' . ($sort2 == 'asc' ? 'arrow-down-wide-short' : 'arrow-up-short-wide');
+            $class .= " $sort2now";
+
+            $afterhtml .= sprintf($sorticonbase, $icon2);
+        }
+
         $sort1  = 'desc';
         if (in_array($colname,
                     array('project', 'tasktype', 'category', 'openedby', 'assignedto')))
@@ -200,11 +215,11 @@ function tpl_list_heading($colname, $format = "<th%s>%s</th>")
 	unset($params['do']);
 	unset($params['project']);
 	unset($params['switch']);
-	# resorting a search result should show always the first results 
+	# resorting a search result should show always the first results
         unset($params['pagenum']);
-	
-	$html = sprintf('<a title="%s" href="%s">%s</a>',
-		eL('sortthiscolumn'), Filters::noXSS(createURL('tasklist', $proj->id, null, $params )), $html);
+
+	$html = sprintf('<a title="%s" href="%s">%s</a>%s',
+		eL('sortthiscolumn'), Filters::noXSS(createURL('tasklist', $proj->id, null, $params )), $html, $afterhtml);
 
 	return sprintf($format, ' class="'.$class.'"', $html);
 }

themes/CleanFS/theme.css
@@ -657,6 +657,28 @@ a {text-decoration: none;}
 }*/
 
 #tasklist_table thead th {line-height:1;}
+
+#tasklist_table thead th .fa,
+#tasklist_table thead th .fas,
+#tasklist_table thead th .fa-solid,
+#tasklist_table thead th .far,
+#tasklist_table thead th .fa-regular,
+#tasklist_table thead th .fa-brand {
+	margin-left: .35em;
+}
+#tasklist_table thead th a .fa:only-child,
+#tasklist_table thead th a .fas:only-child,
+#tasklist_table thead th a .fa-solid:only-child,
+#tasklist_table thead th a .far:only-child,
+#tasklist_table thead th a .fa-regular:only-child,
+#tasklist_table thead th a .fa-brand:only-child
+{
+	margin-left: 0;
+}
+#tasklist_table thead th a + * {
+	color: #999;
+}
+
 #tasklist_table tbody td {border-top:1px solid transparent;border-bottom:1px solid transparent;line-height:1.2;vertical-align:middle;}
 .task_summary {word-wrap:break-word;word-wrap:anywhere;/* css3, ff65+ */}
 #tasklist_table tbody tr:hover td {border-color:#ddd;}

commit ba84bc7f8f3c7b33d68210c2d9d9dcf5a7554ef9

Flyspray/Flyspray #887: Fix task list titles
Fix rev 5b85fc8f which caused some of the task list table header title
text to contain HTML elements.

scripts/index.php
@@ -161,6 +161,7 @@ function tpl_list_heading($colname, $format = "<th%s>%s</th>")
     $sorticonbase = '<span class="fas %s fa-lg"></span>';
     $class   = $colname;
     $html    = '<span>' . eL($colname) . '</span>';
+    $title = el($colname);
     $afterhtml = '';
 /*
     if ($colname == 'comments' || $colname == 'attachments') {
@@ -168,13 +169,13 @@ function tpl_list_heading($colname, $format = "<th%s>%s</th>")
     }
 */
 	if ($colname == 'attachments') {
-		$html='<span class="fas fa-paperclip fa-lg" title="'.$html.'"></span>';
+		$html='<span class="fas fa-paperclip fa-lg" title="'.$title.'"></span>';
 	}
 	if ($colname == 'comments') {
-		$html='<span class="fas fa-comments fa-lg" title="'.$html.'"></span>';
+		$html='<span class="fas fa-comments fa-lg" title="'.$title.'"></span>';
 	}
 	if ($colname == 'votes') {
-		$html='<span class="far fa-star fa-lg" title="'.$html.'"></span>';
+		$html='<span class="far fa-star fa-lg" title="'.$title.'"></span>';
 	}
 
     if (Get::val('order') == $colname) {